976c97f0715f0545d3f3ae0ba11fac37ab7edce0
fatemanazari <fatemanazari78@gmail.com>
2024-06-19 06:52:44 +0330

    feat: Create folders an jsx files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-
+      <Router>
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
     </div>
   );
 }
