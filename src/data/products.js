d75fdf132d2a09717fbd802489c64531f80d004e
fatemanazari <fatemanazari78@gmail.com>
2024-06-19 09:01:07 +0330

    feat: Create products data

---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+import mobile from '../assets/mobile.png';
+import shirt from '../assets/shirt.jpeg';
+import shoes from '../assets/shoes.png';
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "iPhone",
+    price: 1000,
+    productImage: mobile
+  },
+  {
+    id: 2,
+    productName: "Shirt",
+    price: 500,
+    productImage: shirt
+  },
+  {
+    id: 3,
+    productName: "Shoes",
+    price: 1500,
+    productImage: shoes
+  },
+  {
+    id: 1,
+    productName: "iPhone",
+    price: 1000,
+    productImage: mobile
+  },
+  {
+    id: 2,
+    productName: "Shirt",
+    price: 500,
+    productImage: shirt
+  },
+  {
+    id: 3,
+    productName: "Shoes",
+    price: 1500,
+    productImage: shoes
+  }
+]
