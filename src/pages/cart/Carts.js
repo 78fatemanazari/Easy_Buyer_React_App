03982790b5ff71d7cff86f3ca23789b6f8b8223d
fatemanazari <fatemanazari78@gmail.com>
2024-06-27 11:42:28 +0330

    feat: Add cart's functionality

--- src/pages/cart/Carts.js
+++ src/pages/cart/Carts.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PRODUCTS } from '../../data/products';
+import { shopContext } from '../../context/shopContext';
+import Product from '../shop/product';
 
 const Carts = () => {
+    const {cartItems} = useContext(shopContext);
     return (
-        <div>
-            
-        </div>
+        <React.Fragment>
+            <h1>Your Cart Items</h1>
+            <div className='row'>
+                {PRODUCTS.map((p)=>{
+                    if(cartItems.some((i)=> i.id === p.id && i.count>0))
+                        return <Product data={p}></Product>
+                })}
+            </div>
+        </React.Fragment>
     );
 }
 
